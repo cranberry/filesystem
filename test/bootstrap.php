20f4b6edc5436a3cd4569e1cf1943b80afae12c1
Ashur Cabrera <hello@cabreramade.co>
2017-06-21 08:28:35 -0700

    initial commit

---
+++ test/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * This file is part of Fig
+ */
+
+$projectDir = dirname( __DIR__ );
+
+/* Fig autoloading */
+require_once( "{$projectDir}/src/Autoloader.php" );
+Cranberry\Filesystem\Autoloader::register();
